de2dd0cec1709ea576284e004326046122c4b33d
jjh <78027838+beautifulmelon@users.noreply.github.com>
2025-04-28 17:33:50 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ const result = document.getElementById("result")
 
 checkButton.addEventListener("click", function(){
     const name = nameInput.value;
-    if(name == "양수회"){
+    if(name === "양수회"){
         result.innerText = "안녕, 수회야";
-    } else if(name = "정재헌"){
+    } else if(name === "정재헌"){
         result.innerText = "내 이름";
     } else{
         result.innerText = "누구세요?";
